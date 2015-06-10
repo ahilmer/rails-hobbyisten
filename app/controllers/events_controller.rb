2da93a63a8b77e3b4009c8ba188c0ebbed90d13d
Andi Hilmer <andi.hilmer@googlemail.com>
2015-06-10 12:57:50 +0200

    suggestions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
-  before_action :set_event, :authenticate_user!, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
 
