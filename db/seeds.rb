9954173e5c0b2305c5f57d9d9dbedc6cc1697c3e
Andi Hilmer <andi.hilmer@googlemail.com>
2015-06-14 11:53:28 +0200

    layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,10 +69,10 @@ users_locations = UsersLocation.create([user_id: users[0].id, location_id: locat
 
 file = File.open('public/mqdefault.webp')
 
-events = Event.create([ { image: file, title: 'Bolzen1', description: 'Hobby Kick', explicit_location: 'Maximilianstraße 1', max_participants: -1, location_id: locations[0].id, creator_id: users[0].id},
-                        { image: file, title: 'Bolzen2', description: 'Hobby Kick', explicit_location: 'Maximilianstraße 1', max_participants: -1, location_id: locations[0].id, creator_id: users[0].id},
-                        { image: file, title: 'Bolzen3', description: 'Hobby Kick', explicit_location: 'Maximilianstraße 1', max_participants: -1, location_id: locations[0].id, creator_id: users[0].id},
-                        { image: file, title: 'Handball', description: 'Handball Freizeit Treff', explicit_location: 'Maximilianstraße 1', max_participants: -1, location_id: locations[1].id, creator_id: users[0].id}]);
+events = Event.create([ { take_place_timestamp: DateTime.new(2001,2,3,4,5,6,'+7') , image: file, title: 'Bolzen1', description: 'Hobby Kick', explicit_location: 'Maximilianstraße 1', max_participants: -1, location_id: locations[0].id, creator_id: users[0].id},
+                        { take_place_timestamp: DateTime.new(2001,2,3,4,5,6,'+7'), image: file, title: 'Bolzen2', description: 'Hobby Kick', explicit_location: 'Maximilianstraße 1', max_participants: -1, location_id: locations[0].id, creator_id: users[0].id},
+                        { take_place_timestamp: DateTime.new(2001,2,3,4,5,6,'+7'), image: file, title: 'Bolzen3', description: 'Hobby Kick', explicit_location: 'Maximilianstraße 1', max_participants: -1, location_id: locations[0].id, creator_id: users[0].id},
+                        { take_place_timestamp: DateTime.new(2001,2,3,4,5,6,'+7'),image: file, title: 'Handball', description: 'Handball Freizeit Treff', explicit_location: 'Maximilianstraße 1', max_participants: -1, location_id: locations[1].id, creator_id: users[0].id}]);
 
 events_hobbies = EventsHobby.create([event_id: events[0].id, hobby_id: hobbies[0].id ])
 events_hobbies = EventsHobby.create([event_id: events[1].id, hobby_id: hobbies[0].id ])
