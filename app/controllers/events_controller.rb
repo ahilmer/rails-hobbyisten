6bce314a4faff163a43117a777ef125f4a5ed149
Andi Maier <froggythep@googlemail.com>
2015-06-17 17:36:44 +0200

    Added Edit-Event Page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,21 +12,25 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
     @comments = Comment.where(:event_id => params[:id])
-    @new_comment = Comment.new
     @hobbies = Hobby.all
     @events_hobbies = EventsHobby.where(:event_id => params[:id])
     @users_events = UsersEvent.where(:event_id => params[:id])
     @users= User.all
+    @new_comment = Comment.new
   end
 
   def new
-    @new_comment = Comment.new
   end
+  
   def edit
+    @event = Event.find(params[:id])
+    @events_hobbies = EventsHobby.where(:event_id => params[:id])
+    @users_events = UsersEvent.where(:event_id => params[:id])
+    @hobbies = Hobby.all
+    @users= User.all
   end
 
   def create
-    @new_comment = Comment.new
     @new_comment = Comment.new(comment_params)
     if @new_comment.save
       redirect_to event_path(params[:comment][:event_id])
@@ -36,8 +40,12 @@ class EventsController < ApplicationController
   end
 
   def update
-    @event.update(event_params)
-    respond_with(@event)
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to events_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -48,13 +56,13 @@ class EventsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:user_id, :event_id, :message)
   end
+  
+  def event_params
+    params.require(:event).permit(:title, :description, :take_place_timestamp, :explicit_location, :max_participants)
+  end
 
   private
     def set_event
       @event = Event.find(params[:id])
-    end
-
-    def event_params
-      params[:event]
     end
 end
