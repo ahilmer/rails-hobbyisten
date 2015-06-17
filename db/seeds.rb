68b399d2879e774c225ee8c8d95b4988faa2d819
Andi Hilmer <andi.hilmer@googlemail.com>
2015-06-17 14:40:02 +0200

    suggestion user pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ default = File.open('app/assets/images/profiles/defaultprofile.png')
 andi = File.open('app/assets/images/profiles/1.jpg')
 
 users = User.create!([
-{firstname: 'Testing', lastname:'Tester', image: default, :email => "test@test.de", :password => "test1234", :password_confirmation => "test1234" }, 
+{firstname: 'Testing', lastname:'Tester', image: default, :email => "test@test.de", :password => "test1234", :password_confirmation => "test1234" },
 {firstname: 'Andi', lastname:'Maier',image: andi, :email => "Andreas.Maier@hobbyisten.de", :password => "testtest", :password_confirmation => "testtest" },
 {firstname: 'Markus', lastname:'Schöllner',image: default, :email => "Markus.Schoellner@hobbyisten.de", :password => "test1234", :password_confirmation => "test1234" },
 {firstname: 'Andreas', lastname:'Hilmer',image: default, :email => "Andreas.Hilmer@hobbyisten.de", :password => "test1234", :password_confirmation => "test1234" },
